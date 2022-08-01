650d1c58cf33255697526e5fe65f1dacc597ed82
Leonardo Ferreira de Abreu <leonardofabreu@hotmail.com>
2022-08-01 19:04:16 -0300

    adiciona correcao Lint

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
 class Login extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     isUser: true
+  //   };
+  // }
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    return (
+      <form>
+        <div data-testid="page-login">
+          {/* <label htmlFor="login">
+            Login:
+            <input
+              data-testid="login-name-input"
+              type="text"
+              name="login"
+            />
+          </label>
+        </div>
+        <div className="container">
+          <label htmlFor="entrar">
+            Entrar:
+            <input
+              data-testid="login-submit-button"
+              type="submit"
+              name="entrar"
+            />
+          </label> */}
+        </div>
+      </form>
+    );
   }
 }
 
