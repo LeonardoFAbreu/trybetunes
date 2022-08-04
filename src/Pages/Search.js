d5fcda835ae8e27722b71777faafeb6e41e57209
Leonardo Ferreira de Abreu <leonardofabreu@hotmail.com>
2022-08-04 19:58:06 -0300

    adiciona requisito 5

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,15 +1,71 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import Header from './Header';
 
-class Search extends React.Component {
+class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      banda: '',
+      pesquisar: false,
+    };
+  }
+
+  handleSearch = async (event) => {
+    event.preventDefault();
+
+    const { banda } = this.state;
+    // const { history } = this.props;
+
+    this.setState({ pesquisar: true });
+    await createUser({ name: banda });
+
+    // history.push('/search');
+    // PRECISO ENTENDER MELHOR SOBRE O HISTORY
+  }
+
+  handleChangeInput = (event) => {
+    this.setState({
+      banda: event.target.value,
+    });
+  }
+
   render() {
+    const { banda, pesquisar } = this.state;
+    const MIN_LOGIN_LENGTH = 2;
+
+    if (pesquisar) return <p> Carregando...</p>;
+
     return (
       <section>
-        <div data-testid="page-search">
-          Search
-        </div>
-        <Header />
+        {/* <form> */}
+        <form onSubmit={ this.handleSubmit }>
+          <Header />
+          <label htmlFor="Banda>">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="banda"
+              placeholder="Digite o nome da banda"
+              value={ banda }
+              onChange={ this.handleChangeInput }
+            />
+          </label>
+          <div>
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              name="pesquisar"
+              value="pesquisar"
+              disabled={ banda.length < MIN_LOGIN_LENGTH }
+            >
+              <p>Pesquisar</p>
+            </button>
+          </div>
+          {/* <div data-testid="page-search">
+            Search
+          </div> */}
+        </form>
       </section>
     );
   }
