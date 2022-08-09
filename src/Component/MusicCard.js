2d92b10b374ee8839871e679f40b3b7095462ae8
Leonardo Ferreira de Abreu <leonardofabreu@hotmail.com>
2022-08-09 15:19:34 -0300

    adiciona requisito 8

--- src/Component/MusicCard.js
+++ src/Component/MusicCard.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  handleChange = async () => {
+    const { trackId, trackName, previewUrl } = this.props;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    await addSong({ trackId, trackName, previewUrl });
+
+    this.setState({
+      isLoading: false,
+    });
+  };
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { isLoading } = this.state;
+    const { trackName, previewUrl, trackId } = this.props;
 
     return (
-      <div>
-        <h3>{trackName}</h3>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-        </audio>
-      </div>
+      <section>
+        <div>
+          <h3>{trackName}</h3>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            <code>audio</code>
+          </audio>
+
+          {isLoading && <p>Carregando...</p>}
+
+        </div>
+        <div>
+          <label htmlFor="Favorite>">
+            Favorita
+            <input
+              type="checkbox"
+              onChange={ this.handleChange }
+              data-testid={ `checkbox-music-${trackId}` }
+            />
+          </label>
+        </div>
+      </section>
     );
   }
 }
@@ -22,6 +60,7 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
 };
 
 export default MusicCard;
